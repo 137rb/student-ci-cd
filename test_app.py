f2cbac2727796267da4dfb0cf1c2433b4acca581
137rb <6brakhman@gmail.com>
2026-04-21 15:40:33 +0500

    Update logic to multiply

--- test_app.py
+++ test_app.py
@@ -2,13 +2,13 @@ import pytest
 from app import add
 
 def test_add_positive():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 6
 
 def test_add_negative():
-    assert add(-1, -1) == -2
+    assert add(-1, -1) == 1
 
 def test_add_float():
-    assert add(2.5, 2.5) == 5.0
+    assert add(2.5, 2.5) == 6.25
 
 def test_add_invalid():
     with pytest.raises(ValueError):
