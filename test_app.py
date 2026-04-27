3a05568c86ae1270e48380a070e851cedfea7ac2
137rb <6brakhman@gmail.com>
2026-04-27 14:14:45 +0500

    test changed

--- test_app.py
+++ test_app.py
@@ -13,6 +13,4 @@ def test_add_float():
     list3 = [1.1, 2.2, 3.3, 4.4, 5.5, 6.6]
     assert add(list3) == 586.971
 
-def test_add_invalid():
-    with pytest.raises(ValueError):
-        add("2", 3)
+
